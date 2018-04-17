f42d8e6225d3792fd4af5f3a60fdd5ddcda9fb6e
John DeJesus <j.dejesus22@gmail.com>
2018-04-16 22:15:47 -0400

    Add links and start creating loop for links

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -17,6 +17,27 @@ username.send_keys("username")
 password.send_keys("password")
 driver.find_element_by_xpath("//button[@type='submit']").click()
 
+#list of links
+week_urls=['https://data.world/makeovermonday/2018-w-1-u-s-per-capita-consumption-of-poultry-livestock/discuss/2018-w1-u-s-per-capita-consumption-of-poultry-livestock/69848',
+            'https://data.world/makeovermonday/2018-w-2-looks-vs-personality/discuss/2018-w2-looks-vs-personality/71005',
+            'https://data.world/makeovermonday/2018-w-3-u-s-household-income-distribution-by-state/discuss/2018-w3-u-s-household-income-distribution-by-state/72874',
+            'https://data.world/makeovermonday/2018-w-4-turkey-vulture-migration-in-north-and-south-america/discuss/2018-w4-turkey-vulture-migration-in-north-and-south-america/74472',
+            'https://data.world/makeovermonday/2018-w-5-what-the-most-profitable-companies-make-per-second/discuss/2018-w5-what-the-most-profitable-companies-make-per-second/76138',
+            'https://data.world/makeovermonday/2018-w-6-baseball-demographics-1947-2016/discuss/2018-w6-baseball-demographics-1947-2016/78179',
+            'https://data.world/makeovermonday/2018w7-the-winter-olympics/discuss/2018-w7-the-winter-olympics/80806',
+            'https://data.world/makeovermonday/2018w8-where-does-your-medicine-come-from/discuss/2018-w8-where-does-your-medicine-come-from/82935',
+            'https://data.world/makeovermonday/2018w9-world-economic-freedom/discuss/2018-w9-world-economic-freedom/85177',
+            'https://data.world/makeovermonday/w102018-what-policymakers-know-about-women-and-girl-issues/discuss/2018-w10-what-policymakers-know-about-women-and-girl-issues/86558',
+            'https://data.world/makeovermonday/2018w11-growth-in-irish-whiskey-sales/discuss/2018-w11-growth-in-irish-whiskey-sales/90031',
+            'https://data.world/makeovermonday/2018w12-uk-pet-population-in-2017/discuss/2018-w12-uk-pet-population-in-2017/92656',
+            'https://data.world/makeovermonday/what-is-the-uks-favorite-chocolate-bar/discuss/2018-w13-what-is-the-uks-favorite-chocolate-bar/95666',
+            'https://data.world/makeovermonday/2018w14-world-wine-production/discuss/2018-w14-world-wine-production/98310',
+            'https://data.world/makeovermonday/2018w15-arctic-sea-ice-extent/discuss/2018-w15-arctic-sea-ice-extent/100922'
+            ]
+
+#for week_url in week_urls
+#   driver.get(week_url)
+
 #go to url
 time.sleep(5)
 url='https://data.world/makeovermonday/what-is-the-uks-favorite-chocolate-bar/discuss/2018-w13-what-is-the-uks-favorite-chocolate-bar/95666'
